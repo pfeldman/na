ad485da93940b0fb5a2960128cd0a218a3e26a77
Pablo Feldman <pablofeldman90@gmail.com>
2014-07-01 10:17:20 -0300

    Download server code and fix menu

--- assets/js/client/na/NA.WEB.js
+++ assets/js/client/na/NA.WEB.js
@@ -12,64 +12,6 @@ Util.extend(NA.WEB, NA, {
             dom : $('nav > .menu')
         })
         
-        
-        /*
-         * Buttons configuration Example
-         * 
-         */
-        
-        var newBut = new NA.UI.Element.Button({
-            dom : $(".ui-normal")
-        }).render();
-        
-        var group = new NA.UI.Element.Button({
-            dom : $(".checkGroup"),
-            buttonset : true
-        }).render();
-        
-        var iconsOnly = new NA.UI.Element.Button({
-            dom : $(".ui-icons.ui-notext"),
-            icon : "ui-icon-locked",
-            showText : false
-        }).render();
-        
-        var iconsAndText = new NA.UI.Element.Button({
-            dom : $(".ui-icons.ui-text"),
-            icon : "ui-icon-gear"
-        }).render();
-        
-        
-        /*
-         * Accordion Configutation Example
-         * 
-         */
-        
-        var accordion = new NA.UI.Element.Accordion({
-            dom : $("#accordion")
-        }).render();
-        
-        
-        /*
-         * Datepicker Configutation Example
-         * 
-         */
-        
-        var datepicker = new NA.UI.Element.DatePicker({
-            dom : $("#datepicker")
-        }).render();
-       
-        
-        /*
-         * Combobox Configutation Example
-         * 
-         */
-        
-        var combobox = new NA.UI.Element.Combobox({
-            dom : $("#combobox")
-        }).render();
-        
-        
-        
         var buttons = $('.buttons');
         if(buttons.length > 0)
         {
